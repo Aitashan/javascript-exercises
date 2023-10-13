ab829d8905dd8ebc3398be72c573e5243765c9d2
ait <m.aitashan@gmail.com>
2023-10-13 18:50:21 +0500

    completed ex 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let y1 = year % 4
+    let y2 = year % 100
+    let y3 = year % 400
+    if ((y1 === 0 && y2 !== 0) || y3 === 0) {
+        return true
+    } 
+    else {return false}
 
 };
 
