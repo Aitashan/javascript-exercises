2e022e0e85fa347f3d044534a7f2b70b9519c840
ait <m.aitashan@gmail.com>
2023-10-13 15:15:58 +0500

    completed ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+//let num = parseInt(prompt("Enter number of repitition: ", 3))
+//let text = prompt("Enter text to be repeated: ", "Hey")
 
+const repeatString = function(string, num) {
+    if (num === 0) {return ""}
+    else if (num < 0) {return "ERROR"}
+    else {
+        let rText = ""
+        for (x=1; x<=num; x++) {
+         rText += string    
+        }
+        return rText;
+    }
+    
 };
 
 // Do not edit below this line
