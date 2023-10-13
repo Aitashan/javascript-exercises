707d662599509998b80699a7eb255a38dfb4d3a9
ait <m.aitashan@gmail.com>
2023-10-13 19:34:46 +0500

    completed ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let C = (temp - 32) * (5/9)
+  return C === parseInt(C) ? +C : +C.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let F = ((9/5)* temp) + 32
+  return F === parseInt(F) ? +F : +F.toFixed(1)
 };
 
 // Do not edit below this line
