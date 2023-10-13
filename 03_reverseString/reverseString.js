1497e1d121b890ce0500fb4b7765858beef69292
ait <m.aitashan@gmail.com>
2023-10-13 15:44:16 +0500

    completed ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let txtArr = text.split("")
+    let text1 = ""
+    for (i = txtArr.length - 1; i >= 0; i--) {
+        text1 += txtArr[i]
+    }
+    return text1
 };
 
 // Do not edit below this line
