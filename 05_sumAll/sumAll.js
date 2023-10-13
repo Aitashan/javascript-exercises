31d8f6f62ac53784a84b2f4604e6d8b65b54709b
ait <m.aitashan@gmail.com>
2023-10-13 18:16:52 +0500

    completed ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 >= 0 && num2 >= 0 && typeof num1 === "number" && typeof num2 === "number") {
+        if (num1 > num2) {
+            [num1, num2] = [num2, num1]
+        }
+        let sum = 0
+        for (i = num1; i <= num2; i++) {
+            sum += i
+        }
+        return sum
+    }
+    else {return "ERROR"}
 };
 
 // Do not edit below this line
